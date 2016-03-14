67eb7cf56eaf64ca81b193ae95af3682be89a797
san3091 <san3091@gmail.com>
2016-03-14 18:20:18 -0400

    chenge resources path to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  namespace :api, constraints: { subdomain: "api" }, path: '/v1' do
+  namespace :api, constraints: { subdomain: "api" }, path: '/' do
     resources :appointments
   end
   # The priority is based upon order of creation: first created -> highest priority.
--- spec/requests/appointments_spec.rb
+++ spec/requests/appointments_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Appointments", type: :request do
   describe "GET /appointments" do
     it "works! (now write some real specs)" do
-      get appointments_path
+      get api_appointments_path
       expect(response).to have_http_status(200)
     end
   end
