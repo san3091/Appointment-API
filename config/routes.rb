647b4a45d2c5c3a094b190b1136be3d08b0d4a3d
san3091 <san3091@gmail.com>
2016-03-14 18:17:00 -0400

    install rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :appointments
+
+  namespace :api, constraints: { subdomain: "api" }, path: '/v1' do
+    resources :appointments
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
