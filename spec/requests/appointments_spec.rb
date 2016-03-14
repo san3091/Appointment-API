66af10e441feaf1f4e48a40f8f5cfb536897672f
san3091 <san3091@gmail.com>
2016-03-14 19:20:12 -0400

    write red test for show

--- spec/requests/appointments_spec.rb
+++ spec/requests/appointments_spec.rb
@@ -2,12 +2,26 @@ require 'rails_helper'
 
 RSpec.describe "Appointments", type: :request do
   before { host! "api.example.com" }
-
+  
   describe "GET /appointments" do
+    before { Appointment.create!(start_time: Time.parse("11/1/13 7:00"), end_time: Time.parse("11.1.13 8:00")) }
 
-    it "works! (now write some real specs)" do
-      get api_appointments_path
+    it "retrieves all appointments" do
+      get "/appointments"
       expect(response).to have_http_status(200)
+      expect(response.body).not_to be_empty
     end
   end
+
+  describe "GET /appointments/:id" do
+    before { @appointment = Appointment.create!(start_time: Time.parse("11/1/13 9:00"), end_time: Time.parse("11.1.13 9:30")) }
+
+    it "retrieves the appointment by id" do
+      get "/appointments/#{@appointment.id}"
+      expect(response).to have_http_status(200)
+      appointments = JSON.parse(response.body, symbolize_names: true)
+      p appointments
+    end
+  end
+
 end
