d69f8d8d271b0ed87e6f00b36d0e1245ed3ff542
san3091 <san3091@gmail.com>
2016-03-14 20:12:27 -0400

    turns out Active Support is doing the parsing already, no need for time parsing

--- spec/requests/post_appointments_spec.rb
+++ spec/requests/post_appointments_spec.rb
@@ -5,13 +5,20 @@ RSpec.describe "Appointments", type: :request do
 
   describe "POST /appointments" do
 
-    it "creates an appointment with Time objects" do
+    it "creates an appointment" do
       post '/appointments',
+      { 
+        appointment:
         { 
-          appointment:
-            { start_time: Time.parse("11/1/13 9:00"), end_time: Time.parse("11.1.13 9:30") }
-        }.to_json,
-        { "Accept" => Mime::JSON, "Content-Type" => Mime::JSON.to_s }
+          start_time: "11/1/13 9:00", 
+          end_time: "11.1.13 9:30"
+        }
+      }.to_json,
+      { 
+        "Accept" => Mime::JSON, 
+        "Content-Type" => Mime::JSON.to_s 
+      }
+
       expect(response).to have_http_status(201)
 
       appointment = JSON.parse(response.body, symbolize_names: true)
